7d446c20989c238c57c2eb466966ff60f61e6846
abrarnabi <117633785+Abrar-Nabi@users.noreply.github.com>
2025-03-09 19:53:18 +0530

    Update LogoPage.js

--- frontend/src/components/LogoPage.js
+++ frontend/src/components/LogoPage.js
@@ -1,27 +1,44 @@
-import React from "react";
-import "../styles/carBrands.css"; // Ensure you create this CSS file
+import React, { useState } from "react";
+import "../styles/carBrands.css";
+import QuoteForm from "../components/QuoteForm"; // Import QuoteForm
 
 const carBrands = [
-  "Aston-Martin", "Audi", "BMW", "Chevrolet", "Ferrari", "Ford", "Hyundai",
-  "Isuzu", "Jeep", "kia", "Lamborghini", "Lexus", "maserati", "Mazda", 
-  "Mercedes-Benz", "Mitsubishi", "Nissan", "Porsche", "Renault", "Subaru", 
-  "Suzuki", "Skoda","Tesla", "Toyota", "Volvo"
+  "Aston-Martin", "Audi", "BMW", "bentley", "Chevrolet", "Ferrari", "Ford", 
+  "honda", "Hyundai", "Isuzu", "jaguar", "Jeep", "kia", "landrover", "Lexus", 
+  "maserati", "Mazda", "Mercedes-Benz", "Mini", "Mitsubishi", "Nissan", 
+  "Porsche", "Renault", "Subaru", "Suzuki", "Skoda","Toyota", "Volkswagen", "Volvo"
 ];
 
 const CarBrands = () => {
+  const [showQuoteForm, setShowQuoteForm] = useState(false);
+  const [selectedBrand, setSelectedBrand] = useState("");
+
+  const handleBrandClick = (brand) => {
+    setSelectedBrand(brand);
+    setShowQuoteForm(true);
+  };
+
   return (
     <section className="car-brands">
-      
+      <h2 className="brands-heading">Popular Car Brands</h2>
       <div className="brands-container">
         {carBrands.map((brand, index) => (
           <img
             key={index}
-            src={require(`../assets/icons/${brand}.png`)} 
+            src={require(`../assets/icons/${brand}.png`)}
             alt={brand}
             className="brand-logo"
+            onClick={() => handleBrandClick(brand)}
           />
         ))}
       </div>
+
+      {showQuoteForm && (
+        <QuoteForm
+          onClose={() => setShowQuoteForm(false)}
+          selectedBrand={selectedBrand}
+        />
+      )}
     </section>
   );
 };
